c452364c1297dd6e2edda71acba8af9551dbba74
qq17915797 <179157977@qq.com>
2014-11-03 20:28:51 +0800

    修复请求过多界面卡顿问题

--- src/com/minephone/network/NetAccess.java
+++ src/com/minephone/network/NetAccess.java
@@ -1,7 +1,6 @@
 package com.minephone.network;
 
 import java.io.File;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -10,7 +9,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.app.ActivityManager;
-import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.graphics.Bitmap;
@@ -19,7 +18,6 @@ import android.graphics.BitmapFactory;
 import android.net.http.AndroidHttpClient;
 import android.os.Environment;
 import android.text.TextUtils;
-import android.util.Log;
 import android.widget.ImageView;
 
 import com.android.volley.Cache.Entry;
@@ -55,7 +53,7 @@ public class NetAccess {
 	private static final int IMAGEMAXMEASURE = 750;// 图片最大尺寸,0则不限制
 
 	private static final String CachePath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/mquery";// volley缓存目录
-	
+
 	private static RequestQueue mRequestQueue;// 主要的请求类
 
 	private static ImageLoader mImageLoader;// 图片加载器
@@ -66,31 +64,21 @@ public class NetAccess {
 
 	private Context mcontext;
 
+	private Map<String, String> heads;// 提交头
+
+	private Map<String, String> params;// 提交的参数
+
 	private String flag;// 请求标示
-	
-	private Map<String, String> heads;// 提交头(set)
 
-	private Map<String, String> params;// 提交的参数(set)
+	private String cookies;// 提交的cookies
 
-	public String cookies;// 提交的cookies
+	private int timeout = 9000;// volley超时时间默认25秒
 
-
-	private int timeout = 25000;// volley超时时间默认25秒
-
-	
-//	private ProgressDialog mdialog;// 加载窗口
-	private AlertDialog mdialog;// 加载窗口
+	private Dialog mdialog;// 加载窗口
 
 	private NetAccessListener listener;// 回调方法
 
 	private JsonRequest request;// vollery自定义请求类
-	
-	private String url;//请求url
-	private boolean byCache;
-	private boolean byUntilSuccess;
-	private int method;
-	private NetAccessListener listenerbak;//传进来的最原始的回调方法(备份)
-	
 
 	public NetAccess(Context context) {
 		mcontext = context;
@@ -183,7 +171,7 @@ public class NetAccess {
 	}
 
 	private static void checkVar(Context context) {
-		// 网络请求类初始化 context=null时是图片初始化(每次)
+		// 网络请求类初始化
 		if (mRequestQueue == null) {
 			if (android.os.Environment.getExternalStorageState().equals(
 					android.os.Environment.MEDIA_MOUNTED)) {
@@ -194,7 +182,8 @@ public class NetAccess {
 				if (android.os.Build.VERSION.SDK_INT >= 9) {
 					stack = new HurlStack();
 				} else {
-					stack = new HttpClientStack(AndroidHttpClient.newInstance("volley/0"));
+					stack = new HttpClientStack(
+							AndroidHttpClient.newInstance("volley/0"));
 				}
 				Network network = new BasicNetwork(stack);
 				mRequestQueue = new RequestQueue(cache, network);
@@ -217,7 +206,9 @@ public class NetAccess {
 				cacheSize = maxMemory / 8;
 			} else {
 				// Use 1/8th of the available memory for this memory cache.
-				int memClass = ((ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE)).getMemoryClass();
+				int memClass = ((ActivityManager) context
+						.getSystemService(Context.ACTIVITY_SERVICE))
+						.getMemoryClass();
 				cacheSize = 1024 * 1024 * memClass / 8;// 0.5>>50331648
 			}
 			mLruCache = new BitmapLruCache(cacheSize);
@@ -225,11 +216,6 @@ public class NetAccess {
 		// 图片(缓存)请求类初始化
 		if (mImageLoader == null) {
 			mImageLoader = new ImageLoader(mRequestQueue, mLruCache);
-		}
-		
-		if (context!=null) {//是网络初始化，不是图片初始化
-			//网络模块初始化完成，提交没有完成的,未实现
-			
 		}
 	}
 
@@ -283,12 +269,15 @@ public class NetAccess {
 							opts.inJustDecodeBounds = false;
 							obj = BitmapFactory.decodeByteArray(cachedata.data,0, cachedata.data.length, opts);
 						} else {
-							obj = BitmapFactory.decodeByteArray(cachedata.data,0, cachedata.data.length);
+							obj = BitmapFactory.decodeByteArray(cachedata.data,
+									0, cachedata.data.length);
 						}
 					} else {
 						obj = JsonRequest.decode(cachedata.data);
 					}
+
 					result = (T) obj;
+
 				}
 			}
 		} catch (Exception e) {
@@ -351,7 +340,7 @@ public class NetAccess {
 		this.flag = flag;
 		return this;
 	}
-	
+
 	/**
 	 * 设置请求cookies
 	 * 
@@ -364,11 +353,10 @@ public class NetAccess {
 		return this;
 	}
 
-	public NetAccess setDialog(AlertDialog dialog) {
+	public NetAccess setDialog(Dialog dialog) {
 		mdialog = dialog;
 		return this;
 	}
-	
 	
 	/**
 	 * 设置显示加载中Dialog窗口
@@ -383,7 +371,9 @@ public class NetAccess {
 
 	public NetAccess showDialog(String tip, boolean canCancel) {
 		if (mdialog!=null) {
-			mdialog.dismiss();
+			if (mdialog.isShowing()) {
+				mdialog.dismiss();				
+			}
 			mdialog.show();
 		} else {
 			if (tip == null) {
@@ -393,8 +383,19 @@ public class NetAccess {
 			}
 		}
 		return this;
+//		if (!(mdialog == null || !mdialog.isShowing())) {
+//			mdialog.dismiss();
+//		}
+//		if (tip == null) {
+//			mdialog = ProgressDialog.show(mcontext, null, "加载中", true,
+//					canCancel, null);
+//		} else {
+//			mdialog = ProgressDialog.show(mcontext, null, tip, true, canCancel,
+//					null);
+//		}
+//		return this;
 	}
-
+	
 	/**
 	 * 设置超时时间，默认20秒
 	 * 
@@ -406,7 +407,7 @@ public class NetAccess {
 		this.timeout = timeout;
 		return this;
 	}
-	
+
 	/**
 	 * 开始get请求
 	 * 
@@ -415,10 +416,10 @@ public class NetAccess {
 	 * @param listener
 	 */
 	synchronized public void byGet(String url, NetAccessListener listener) {
-		this.method = Method.GET;
-		this.url = url;
-		this.listener = listener;
-		startrequest();
+		url += getParamStr(params);
+
+		MQLog.i(TAG, "gurl-->" + url);
+		startrequest(url, Method.GET, false, listener);
 	}
 
 	/**
@@ -429,48 +430,22 @@ public class NetAccess {
 	 * @param listener
 	 */
 	synchronized public void byCacheGet(String url, NetAccessListener listener) {
-		this.method = Method.GET;
-		this.byCache = true;
-		this.byUntilSuccess = false;
-		this.url = url;
-		this.listener = listener;
-		startrequest();
-	}
-
-	/**
-	 * 直到成功的get请求,如果失败保存到数据库,调用dealfailrequest方法重新请求
-	 * @author ping
-	 * @create 2014-8-29 下午12:24:08
-	 * @param url
-	 * @param listener
-	 */
-	synchronized public void byUntilSuccessGet(String url, final NetAccessListener listener) {
-		this.method = Method.GET;
-		this.byCache = false;
-		this.byUntilSuccess = true;
-		this.url = url;
-		this.listener = listener;
-		startrequest();
-	}
-
-	/**
-	 * 重新请求上次未成功的请求
-	 * @param listener
-	 */
-	public void dealFailRequest(NetAccessListener listener) {
-		ArrayList<RqData> list = USRequestUtil.listall(mcontext);
-		for (RqData data:list) {
-			this.params = data.postdata;
-			this.heads = data.headdata;
-			if (data.method==Method.GET) {
-				byUntilSuccessGet(data.url, listener);
-			} else {
-				byUntilSuccessPost(data.url, listener);
+		url += getParamStr(params);
+		String cache = getCache(url);
+		if (cache != null) {
+			MQLog.i(TAG, "gcache-->" + cache);
+			if (!(mdialog == null || !mdialog.isShowing())) {
+				mdialog.dismiss();
 			}
-			USRequestUtil.del(mcontext, data.id);
+			if (listener != null) {
+				listener.onAccessComplete(true, cache, null, flag);
+			}
 		}
+
+		MQLog.i(TAG, "cgurl-->" + url);
+		startrequest(url, Method.GET, true, listener);
 	}
-	
+
 	/**
 	 * 开始post 请求
 	 * 
@@ -479,12 +454,8 @@ public class NetAccess {
 	 * @param listener
 	 */
 	synchronized public void byPost(String url, NetAccessListener listener) {
-		this.method = Method.POST;
-		this.byCache = false;
-		this.byUntilSuccess = false;
-		this.url = url;
-		this.listener = listener;
-		startrequest();
+		MQLog.i(TAG, "purl-->" + url + getParamStr(params));
+		startrequest(url, Method.POST, false, listener);
 	}
 
 	/**
@@ -495,114 +466,127 @@ public class NetAccess {
 	 * @param listener
 	 */
 	synchronized public void byCachePost(String url, NetAccessListener listener) {
-		this.method = Method.POST;
-		this.byCache = true;
-		this.byUntilSuccess = false;
-		this.url = url;
-		this.listener = listener;
-		startrequest();
+		String cache = getCache(url);
+		if (cache != null) {
+			MQLog.i(TAG, "pcache-->" + cache);
+			if (!(mdialog == null || !mdialog.isShowing())) {
+				mdialog.dismiss();
+			}
+			if (listener != null) {
+				listener.onAccessComplete(true, cache, null, flag);
+			}
+		}
+
+		MQLog.i(TAG, "cpurl-->" + url + getParamStr(params));
+		startrequest(url, Method.POST, true, listener);
 	}
 	
 	/**
-	 * 直到成功的post请求,如果失败保存到数据库,调用dealfailrequest方法重新请求
+	 * 直到成功的get请求,如果失败保存到数据库,调用dealfailrequest方法重新请求
+	 * 
 	 * @author ping
 	 * @create 2014-8-29 下午12:24:08
 	 * @param url
 	 * @param listener
 	 */
-	synchronized public void byUntilSuccessPost(String url, final NetAccessListener listener) {
-		this.method = Method.POST;
-		this.byCache = false;
-		this.byUntilSuccess = true;
-		this.url = url;
-		this.listener = listener;
-		startrequest();
+	synchronized public void byUntilSuccessGet(String url,final NetAccessListener listener) {
+		final String murl = url + getParamStr(params);
+		MQLog.i(TAG, "usgurl-->" + url);
+
+		NetAccessListener mlistener = new NetAccessListener() {
+			@Override
+			public void onAccessComplete(boolean success, String object,
+					VolleyError error, String flag) {
+				if (!success) {
+					USRequestUtil
+							.add(mcontext, Method.GET, murl, heads, params);
+				}
+				if (listener != null) {
+					listener.onAccessComplete(success, object, error, flag);
+				}
+			}
+		};
+		startrequest(murl, Method.GET, false, mlistener);
 	}
-	
+
+	/**
+	 * 直到成功的post请求,如果失败保存到数据库,调用dealfailrequest方法重新请求
+	 * 
+	 * @author ping
+	 * @create 2014-8-29 下午12:24:08
+	 * @param url
+	 * @param listener
+	 */
+	synchronized public void byUntilSuccessPost(final String url,final NetAccessListener listener) {
+		MQLog.i(TAG, "uspurl-->" + url);
+
+		NetAccessListener mlistener = new NetAccessListener() {
+			@Override
+			public void onAccessComplete(boolean success, String object,
+					VolleyError error, String flag) {
+				if (!success) {
+					USRequestUtil.add(mcontext, Method.POST, url, heads, params);
+				}
+				if (listener != null) {
+					listener.onAccessComplete(success, object, error, flag);
+				}
+			}
+		};
+		startrequest(url, Method.POST, false, mlistener);
+	}
+
+	/**
+	 * 重新请求上次未成功的请求
+	 * 
+	 * @param listener
+	 */
+	public void dealFailRequest(NetAccessListener listener) {
+		ArrayList<RqData> list = USRequestUtil.listall(mcontext);
+		for (RqData data : list) {
+			this.params = data.postdata;
+			this.heads = data.headdata;
+			if (data.method == Method.GET) {
+				byUntilSuccessGet(data.url, listener);
+			} else {
+				byUntilSuccessPost(data.url, listener);
+			}
+			USRequestUtil.del(mcontext, data.id);
+		}
+	}
+
 	/**
 	 * 开始请求
 	 * 
 	 * @author ping 2014-4-11 下午12:52:26
+	 * @param url
+	 *            请求链接(post不包含参数、get包含参数)
+	 * @param savecache
+	 *            是否缓存
 	 */
-	synchronized private void startrequest() {
-		String totalurl = url+getParamStr(params);
-		if (byUntilSuccess) {
-			if (method == Method.GET) {
-				MQLog.i(TAG, "usgurl-->" + totalurl);
-			} else if (method == Method.POST) {
-				MQLog.i(TAG, "uspurl-->" + totalurl);
-			}
-		} else if (byCache) {
-			if (method == Method.GET) {
-				MQLog.i(TAG, "cgurl-->" + totalurl);
-			} else if (method == Method.POST) {
-				MQLog.i(TAG, "cpurl-->" + totalurl);
-			}
-		} else {
-			if (method == Method.GET) {
-				MQLog.i(TAG, "gurl-->" + totalurl);
-			} else if (method == Method.POST) {
-				MQLog.i(TAG, "purl-->" + totalurl);
-			}
-		}
-		
-		
-		if (mdialog!=null) {
+	private void startrequest(String url, int method,
+			boolean savecache, NetAccessListener listener) {
+		if (mdialog != null) {
 			mdialog.show();
 		}
-		
-		if (byUntilSuccess) {
-			//加入请求列表
-			listenerbak = listener;
-			listener = new NetAccessListener() {
-				@Override
-				public void onAccessComplete(boolean success, String object,VolleyError error, String flag) {
-					if (!success) {
-						USRequestUtil.add(mcontext, method, url,heads, params);
-					}
-					if (listenerbak != null) {
-						listenerbak.onAccessComplete(success, object, error, flag);			
-					}
-				}
-			};
-		} else if (byCache) {
-			String cache = null;
-			if (method == Method.GET) {
-				cache = getCache(totalurl);
-			} else if (method == Method.POST) {
-				cache = getCache(url);
-			}
-			
-			if (cache != null) {
-				if (method == Method.GET) {
-					MQLog.i(TAG, "gcache-->" + cache);
-				} else if (method == Method.POST) {
-					MQLog.i(TAG, "pcache-->" + cache);
-				}
-				if (listener != null) {
-					listener.onAccessComplete(true, cache, null, flag);
-				}
-			}
-		}
-		
-		ResponseListener relistener = new ResponseListener(NetAccess.this);
-		if (method == Method.GET) {
-			request = new JsonRequest(method,  totalurl, relistener, relistener);
-		} else {
-			request = new JsonRequest(method, url, relistener, relistener);
+
+		this.listener = listener;
+
+		ResponseListener relistener = new ResponseListener(this);
+		request = new JsonRequest(method, url, relistener, relistener);
+		request.setHeaders(heads);
+		if (method != Method.GET) {
 			request.setParams(params);
 		}
-		request.setHeaders(heads);
 		if (cookies==null) {
 			cookies = CookieUtil.getCookie(mcontext, GetDomainName(url));
 		}
-		Log.i("test", "post cookice:" + cookies);
 		request.setCookies(cookies);
 		request.setTimeout(timeout);
-		request.setShouldCache(byCache);
+		request.setShouldCache(savecache);
 		request.setTag(TextUtils.isEmpty(flag) ? TAG : flag);
 		mRequestQueue.add(request);
 		mRequestQueue.start();
+		// request.getReHeaders()
 	}
 
 	/**
@@ -621,7 +605,7 @@ public class NetAccess {
 		@Override
 		public void onResponse(String response) {
 			MQLog.i(TAG, "callback-->" + response);
-			
+
 			if (!(net.mdialog == null || !net.mdialog.isShowing())) {
 				net.mdialog.dismiss();
 			}
@@ -686,26 +670,24 @@ public class NetAccess {
 		if (imageview == null) {
 			return;
 		}
-		
-		if (url==null) {
-			imageview.setImageResource(errorimg);
-			return;
-		}
-		
+
 		checkVar(null);
 
 		imageview.setTag(url);
 
-		Bitmap bm = mLruCache.getBitmap(getCacheKey(url, maxmeasure, maxmeasure));
+		Bitmap bm = mLruCache
+				.getBitmap(getCacheKey(url, maxmeasure, maxmeasure));
 		if (bm == null) {
 			bm = getCache(url, Bitmap.class);
 			if (bm != null) {
-				mLruCache.putBitmap(getCacheKey(url, maxmeasure, maxmeasure),bm);
+				mLruCache.putBitmap(getCacheKey(url, maxmeasure, maxmeasure),
+						bm);
 			}
 		}
 
 		if (bm == null) {
-			mImageLoader.get(url, ImageLoader.getImageListener(imageview,loadingimg, errorimg), maxmeasure, maxmeasure);
+			mImageLoader.get(url, ImageLoader.getImageListener(imageview,
+					loadingimg, errorimg), maxmeasure, maxmeasure);
 		} else {
 			final int bmsize = bm.getRowBytes() * bm.getHeight();
 			imageview.setImageBitmap(bm);
@@ -715,15 +697,19 @@ public class NetAccess {
 						public void onResponse(Bitmap bitmap) {
 							if (imageview.getTag().toString().equals(url)) {
 								imageview.setImageBitmap(bitmap);
-								if (bmsize != bitmap.getRowBytes() * bitmap.getHeight()) {
+								if (bmsize != bitmap.getRowBytes()
+										* bitmap.getHeight()) {
 									// 图片改变了，更新图片缓存
-									mLruCache.putBitmap(getCacheKey(url, maxmeasure,maxmeasure), bitmap);
+									mLruCache.putBitmap(
+											getCacheKey(url, maxmeasure,
+													maxmeasure), bitmap);
 								}
 							} else if (!bitmap.isRecycled()) {
 								bitmap.recycle();
 							}
 						}
-					}, maxmeasure, maxmeasure, Config.ARGB_8888,new ErrorListener() {
+					}, maxmeasure, maxmeasure, Config.ARGB_8888,
+					new ErrorListener() {
 						@Override
 						public void onErrorResponse(VolleyError arg0) {
 							imageview.setImageResource(errorimg);
@@ -763,7 +749,9 @@ public class NetAccess {
 	}
 
 	private static String getCacheKey(String url, int maxWidth, int maxHeight) {
-		return (new StringBuilder(url.length() + 12)).append("#W").append(maxWidth).append("#H").append(maxHeight).append(url).toString();
+		return (new StringBuilder(url.length() + 12)).append("#W")
+				.append(maxWidth).append("#H").append(maxHeight).append(url)
+				.toString();
 	}
 
 	private static int calculateSampleSize(BitmapFactory.Options options,
@@ -792,4 +780,5 @@ public class NetAccess {
         }
 		return "";
 	}
+
 }
